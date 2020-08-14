7c80c5905007cb16a474ee4288d8f06d184ba6aa
WomackCodes <chris@womack.codes>
2020-08-14 15:22:15 -0700

    Done

--- js/main.js
+++ js/main.js
@@ -1,46 +1,41 @@
-//  Pseduocode: 
-
-// When the application initially loads, the < input > 's value 
-// should be set to 1 and the initial count of 0 is rendered such 
-// that the display looks something like initial WIREFRAME Photo
-
 
 // ------- Cached Elements: -------
 const addBtn = document.getElementById("add");
 const subBtn = document.getElementById("subtract");
-let runningTotal = document.querySelector("h1").innerText;
-inputValue = document.getElementById("inputBox").innerText;
+let runningTotal = document.querySelector("h1");
+let whatWeNeed = document.querySelector(".input");
+let number;
 
+//Event listener
 
-// ------- Functions -------
+document.querySelector("#add").addEventListener('click', handleAdd);
+document.querySelector("#subtract").addEventListener('click', handleSub);
+
+// Functions
 function init() {
-runningTotal = 0;
-inputValue = 1;
-render();
+    number = 0;
+    whatWeNeed.value = 1;
+    render();
 }
+
 function render() {
-}
-function handleClick(e) {
-    let inputValue = parseInt(inputValue);
-    if (e.target === addBtn && inputValue !== NaN) {
-        runningTotal += parseInt(inputValue);
-        inputValue = "";
-        render();
-    } else if ((e.target === subBtn && inputValue !== NaN)) {
-        runningTotal -= parseInt(inputValue);
-        inputValue = "";
-        render();
-    } else {
-        h1.innerHTML = "Not a number, please try again";
+    if (number >= 0) {
+        runningTotal.textContent = "+" + number;
+        runningTotal.style.color = "green";
+    } else if (number < 0) {
+        runningTotal.textContent = number;
+        runningTotal.style.color = "red";
     }
 }
 
+function handleAdd() {
+    number += parseInt(whatWeNeed.value);
+    render();
+}
+
+function handleSub() {
+    number -= parseInt(whatWeNeed.value);
+    render();
+}
+
 init();
-
-// ------- Event Listeners -------
-
-document.querySelector("button").addEventListener('click', handleClick);
-
-
-// Hover ::before add
-// Hover ::before subtract
